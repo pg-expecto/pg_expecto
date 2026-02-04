5eea9a4f72ac8ba656a47f5106fe1e2e0c987919
Ринат <kznalp@yandex.ru>
2026-02-04 09:28:49 +0300

    Update before_start.sql
    Версия 6

--- sh/load_test/before_start.sql
+++ sh/load_test/before_start.sql
@@ -1,11 +1,20 @@
 -- before_start.sql
--- version 3.1
+-- version 6.0
 --Вариант по умолчанию - НИЧЕГО НЕ МЕНЯТЬ
 SELECT CURRENT_TIMESTAMP ;
 --Вариант по умолчанию - НИЧЕГО НЕ МЕНЯТЬ
 
 --------------------------------------------------------
 -- Изменения/добавления тестовых таблиц
+ALTER TABLE pgbench_history DROP CONSTRAINT IF EXISTS pgbench_history_aid_fkey ; 
+ALTER TABLE pgbench_history DROP CONSTRAINT IF EXISTS pgbench_history_bid_fkey ; 
+ALTER TABLE pgbench_history DROP CONSTRAINT IF EXISTS pgbench_history_tid_fkey ; 
+
+-- 1. Добавить столбец для предотвращения HOT-обновлений
+ALTER TABLE pgbench_history ADD COLUMN IF NOT EXISTS random_fill INTEGER;
+
+-- 2. Создать индекс на этот столбец (обязательно для запрета HOT)
+CREATE INDEX IF NOT EXISTS idx_pgbench_history_random_fill ON pgbench_history(random_fill);
 
 -- Изменения/добавления тестовых таблиц
 --------------------------------------------------------

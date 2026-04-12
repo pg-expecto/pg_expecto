08dc6b06d12fd702d8182adcfb5a6919f8bec6fc
Ринат <kznalp@yandex.ru>
2026-04-12 08:51:06 +0300

    Change license from MIT to Apache License 2.0.

--- sh/load_test/scenario1.oltp.sql
+++ sh/load_test/scenario1.oltp.sql
@@ -1,4 +1,3 @@
--- scenario1.sql
 -- Copyright 2026 Ринат (pg_expecto)
 -- 
 -- Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 -- 
+-- scenario1.sql
 -- 5.3
 -- OLTP
 -- SELECT
